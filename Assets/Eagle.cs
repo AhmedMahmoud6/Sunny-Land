954ba26f091a89f9212120798c655a5de5ef2c96
= <ahmedmahmoudd626@gmail.com>
2023-02-18 19:36:09 +0200

    adding opossum enemy

--- Assets/Eagle.cs
+++ Assets/Eagle.cs
@@ -6,12 +6,19 @@ public class Eagle : MonoBehaviour
 {
     [SerializeField]
     Transform player;
+    [SerializeField]
+    float eagleHeight = 2;
+    [SerializeField]
+    float speed = 2;
     SpriteRenderer sr;
+    Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponentInChildren<SpriteRenderer>();
+        startPosition = transform.position;
+        StartCoroutine(EagleAnimation());
     }
 
     // Update is called once per frame
@@ -24,6 +31,37 @@ public class Eagle : MonoBehaviour
         else
         {
             sr.flipX = false;
+        }
+    }
+
+
+    IEnumerator EagleAnimation()
+    {
+        Vector3 endPosition = new Vector3(startPosition.x,startPosition.y + eagleHeight, startPosition.z);
+
+        float value = 0;
+        bool isFlight = true;
+        
+        while(true)
+        {
+            yield return null;
+
+            if (isFlight )
+            {
+                transform.position = Vector3.Lerp(startPosition,endPosition,value);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(endPosition, startPosition, value);
+
+            }
+            value += Time.deltaTime * speed;
+
+            if (value > 1)
+            {
+                value = 0;
+                isFlight = !isFlight;
+            }
         }
     }
 }
